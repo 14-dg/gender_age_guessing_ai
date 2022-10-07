3adc57bc075cc6499892f4a994a403e2391d01c8
Fabius <111242550+FabiusHerling@users.noreply.github.com>
2022-10-07 19:22:47 +0200

    Warum geht das "DetectMultiscale" nicht?

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ import glob
 
 face_cascade = cv2.CascadeClassifier('haarcascades_models/haarcascade_frontalface_default.xml') # Holt die Daten von cv2, mit denen man die Gesicher erkennen kann
 pictures = glob.glob('./Pictures/*.jpg') # Holt die Bilder (nur .jpg) aus dem Ordner "Pictures"
-for file in pictures[]: # Für jeder Bild
+for file in pictures: # Für jeder Bild
     img = cv2.imread(file, 1)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
     try :
-        for ( x , y , w , h ) in faces :
-            roi_color = img [ y : y + h , x : x + w ]
-        normiert = cv2.resize (roi_color, (128,128))
-        cv2.imwrite (" extracted_faces / " + str (img_number) + ".jpg " , normiert)
+        for ( x , y , w , h ) in faces:
+            gray_face = img[y:y + h, x:x + w]
+        normiert = cv2.resize (gray_face, (128,128))
+        cv2.imwrite ("Faces/" + str(img_number) + ".jpg " , normiert)
     except :
         print ("Kein Gesicht gefunden")
     img_number = img_number + 1
