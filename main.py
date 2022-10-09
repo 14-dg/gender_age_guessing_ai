3f094606ccabaa2c4957cac82295fca1d7e1096e
Fabius <111242550+FabiusHerling@users.noreply.github.com>
2022-10-09 20:50:23 +0200

    Der Fehler bei der Methode ist immer noch da

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ import glob
 
 face_cascade = cv2.CascadeClassifier('haarcascades_models/haarcascade_frontalface_default.xml') # Holt die Daten von cv2, mit denen man die Gesicher erkennen kann
 pictures = glob.glob('./Pictures/*.jpg') # Holt die Bilder (nur .jpg) aus dem Ordner "Pictures"
-for file in pictures: # Für jeder Bild
-    img = cv2.imread(file, 1)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+for file in pictures: # Für jedes Bild
+    img = cv2.imread(file, 1) # Das Bild wird eingelesen
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # Macht die Bilder schwarz weiß für die KI Gesichtserkennung
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5) # Erkennt vorhandene Gesichter in dem Bild
     try :
         for (x, y, w, h) in faces:
-            gray_face = img[y:y + h, x:x + w]
-        normiert = cv2.resize (gray_face, (128,128))
-        cv2.imwrite ("Faces/" + str(img_number) + ".jpg " , normiert)
+            face = img[y:y + h, x:x + w] # Wenn es ein Gesicht gibt, wird das aus dem ursprünglichen Bild raus geschnitten
+        normiert = cv2.resize (face, (128,128)) # Dann wird es auf 128² zugeschnitten (anscheinend später für die KI relevant)
+        cv2.imwrite ("Faces/" + str(img_number) + ".jpg " , normiert) # Dann wird das Gesicht gespeichert
     except :
-        print ("Kein Gesicht gefunden")
+        print ("Kein Gesicht gefunden") # Falls es kein Gesicht gibt, wird die Nachricht gedruckt
     img_number = img_number + 1
