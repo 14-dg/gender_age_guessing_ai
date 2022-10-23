1d51c487ddc65cb7c0134b4ef4342a4b2f244d3d
Daniel <daniel.graef14@gmail.com>
2022-10-23 17:58:53 +0200

    added comments for load_faces_training.py

--- load_faces_training.py
+++ load_faces_training.py
@@ -1,19 +1,22 @@
 import glob
 import cv2
 
-#loads the imgs after knowing where they are located
+
 def get_imgs_from_filenames(pictures)->list:
+    """loads the imgs after knowing where they are located
+    """
     imgs = []
     for file in pictures:
         img = cv2.resize(cv2.imread(file, 1), (128, 128))
         imgs.append(img)
     return imgs
 
-'''
-return imgs in array form
-male -> 1 female -> 0
-'''
+
 def get_pictures_gender()-> tuple[list, list]:
+    """
+    return imgs in array form
+    male -> 1 female -> 0
+    """
     def get_pictures_gender_male()-> tuple[list, list]:
         pictures = glob.glob('./Faces/Training/Gender/male/*.jpg')
         genders = [1]*len(pictures)
@@ -27,9 +30,14 @@ def get_pictures_gender()-> tuple[list, list]:
     pictures_female, genders_female = get_pictures_gender_female()
     return pictures_male+pictures_female, genders_male+genders_female
 
-def get_pictures_age()-> list:
+def get_pictures_age()-> tuple[list, list]:
+    """
+    return imgs in array form
+    ages -> represent the age of the person
+    """
     pictures = glob.glob('./Faces/Training/Age*.jpg')
-    return get_imgs_from_filenames(pictures)
+    ages = []
+    return get_imgs_from_filenames(pictures), ages
 
 if __name__ == "__main__":
     imgs, genders = get_pictures_gender()
